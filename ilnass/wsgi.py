7910431ebd2628ae73e49d0c0d3203815d24cf68
soohyun-lee <soohyun527@gmail.com>
2020-10-06 13:52:23 +0900

    ilnass 초기세팅

--- ilnass/wsgi.py
+++ ilnass/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for class project.
+WSGI config for ilnass project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ilnass.settings')
 
 application = get_wsgi_application()
